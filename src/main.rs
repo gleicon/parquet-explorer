57deebfad2325dcab4519099222ea3399b55fa3f
Gleicon Moraes <gleicon@gmail.com>
2021-06-27 20:58:27 -0300

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use parquet::file::reader::{FileReader, SerializedFileReader};
+use std::{fs::File, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("test_data/taxi_2019_04.parquet");
+    if let Ok(file) = File::open(&path) {
+        let reader = SerializedFileReader::new(file).unwrap();
+        let parquet_metadata = reader.metadata();
+        let row_group_reader = reader.get_row_group(0).unwrap();
+
+        println!("num_row_groups: {}", parquet_metadata.num_row_groups());
+        println!("row_group_reader: {}", row_group_reader.num_columns());
+    }
 }
