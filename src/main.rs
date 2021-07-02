b75aca9fb131015fbd38a0cc0c51ff0a831c30f8
Gleicon Moraes <gleicon@gmail.com>
2021-07-01 22:31:11 -0300

    refactor using executorcontext

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use arrow::record_batch::RecordBatch;
 use arrow::util::pretty::print_batches;
 use clap::{AppSettings, Clap};
-//use datafusion::prelude::*;
+use std::time::Instant;
+
 use datafusion;
 use parquet::file::reader::{FileReader, SerializedFileReader};
 
@@ -28,30 +29,14 @@ pub async fn main() {
 
     if let Ok(file) = File::open(&path) {
         let reader = SerializedFileReader::new(file).unwrap();
+
         if opts.describe {
             describe_parquet(reader);
         }
     }
 
     match opts.query {
-        Some(q) => {
-            println!("query: {}", q);
-            // parquet_query(opts.file, q);
-            // ()
-            // cargo run -- -d -f test_data/taxi_2019_04.parquet -q "SELECT count(*) FROM parquet_tables"
-            let mut ctx = datafusion::prelude::ExecutionContext::new();
-            ctx.register_parquet("parquet_tables", &opts.file).unwrap();
-
-            // create a plan to run a SQL query
-            //    let df = ctx.sql("SELECT a, MIN(b) FROM example GROUP BY a LIMIT 100")?;
-            let df = ctx.sql(&q).unwrap();
-
-            // execute and print results
-            let results: Vec<RecordBatch> = df.collect().await.unwrap();
-            print_batches(&results).unwrap();
-            //Ok(())
-            ()
-        }
+        Some(q) => query_parquet(opts.file, q).await.unwrap(),
         None => println!("Empty query"),
     }
 }
@@ -59,21 +44,25 @@ pub async fn main() {
 fn describe_parquet(reader: parquet::file::reader::SerializedFileReader<std::fs::File>) {
     let parquet_metadata = reader.metadata();
     let row_group_reader = reader.get_row_group(0).unwrap();
+
     println!("num_row_groups: {}", parquet_metadata.num_row_groups());
     println!("row_group_reader: {}", row_group_reader.num_columns());
 }
 
-async fn parquet_query(path: String, query: String) -> datafusion::error::Result<()> {
-    // register the table
+async fn query_parquet(path: String, query: String) -> datafusion::error::Result<()> {
+    let start = Instant::now();
+
+    println!("query: {}", query);
+    // cargo run -- -d -f test_data/taxi_2019_04.parquet -q "SELECT count(*) FROM parquet_tables"
     let mut ctx = datafusion::prelude::ExecutionContext::new();
-    ctx.register_parquet("table", &path)?;
+
+    ctx.register_parquet("parquet_tables", &path).unwrap();
 
     // create a plan to run a SQL query
-    //    let df = ctx.sql("SELECT a, MIN(b) FROM example GROUP BY a LIMIT 100")?;
-    let df = ctx.sql(&query)?;
-
-    // execute and print results
-    let results: Vec<RecordBatch> = df.collect().await?;
-    print_batches(&results)?;
+    let df = ctx.sql(&query).unwrap();
+    let results: Vec<RecordBatch> = df.collect().await.unwrap();
+    print_batches(&results).unwrap();
+    let duration = start.elapsed();
+    println!("Time elapsed: {:?}\n{:?} rows", duration, results.len());
     Ok(())
 }
