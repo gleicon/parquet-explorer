e95d8ab808674847e376b2e59846e393d9d878e6
Gleicon Moraes <gleicon@gmail.com>
2021-07-02 22:49:53 -0300

    first shot at a repl

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use clap::{AppSettings, Clap};
+use linefeed::{Interface, ReadResult};
+use std::iter::FromIterator;
+use std::path::Path;
 
 mod filemanager;
 mod parquethandler;
+mod repl;
 
 #[derive(Clap)]
 #[clap(version = "0.0.1", author = "gleicon <gleicon@gmail.com>")]
@@ -24,6 +28,9 @@ struct Opts {
 
     #[clap(short = 's', long = "singlepartition")]
     singlepartition: bool,
+
+    #[clap(short = 'i', long = "interactive")]
+    interactive: bool,
 }
 
 #[tokio::main]
@@ -36,6 +43,42 @@ pub async fn main() {
         parquet_handler.clone().describe().await;
     }
 
+    if opts.interactive {
+        println!("Interactive mode");
+        let reader = Interface::new("parquet-explorer").unwrap();
+        let path = Path::new(&opts.file);
+        let tablename = path.file_stem().unwrap().to_str().unwrap();
+        println!("data source: {}", tablename);
+
+        let prompt = format!("table: {:?}> ", tablename);
+
+        reader.set_prompt(&prompt).unwrap();
+
+        while let ReadResult::Input(input) = reader.read_line().unwrap() {
+            let res = Vec::from_iter(
+                input
+                    .to_lowercase()
+                    .trim()
+                    .split("something")
+                    .map(String::from),
+            );
+            let cmd = &res[0];
+
+            match cmd.as_str() {
+                "describe" => parquet_handler.clone().describe().await,
+                "select" => parquet_handler
+                    .clone()
+                    .query(input.clone().to_string())
+                    .await
+                    .unwrap(),
+                "" => (),
+                "quit" => return,
+                "exit" => return,
+                _ => println!("Invalid command {:?}", input),
+            };
+        }
+        return;
+    }
     match opts.query {
         Some(q) => {
             if !opts.newfile {
---
+++ src/repl.rs
@@ -0,0 +1,55 @@
+use linefeed::{Interface, ReadResult};
+use std::iter::FromIterator;
+use std::path::Path;
+
+struct Repl {
+    parquet_handler: crate::parquethandler::ParquetHandler,
+    root_path: String,
+}
+
+impl Repl {
+    pub async fn start_interactive_mode(&mut self) {
+        println!("Interactive mode");
+        let reader = Interface::new("parquet-explorer").unwrap();
+        let path = Path::new(&self.root_path);
+        let tablename = path.file_stem().unwrap().to_str().unwrap();
+        println!("data source: {}", tablename);
+
+        let prompt = format!("table: {:?}> ", tablename);
+
+        reader.set_prompt(&prompt).unwrap();
+
+        while let ReadResult::Input(input) = reader.read_line().unwrap() {
+            let res = Vec::from_iter(
+                input
+                    .to_lowercase()
+                    .trim()
+                    .split("something")
+                    .map(String::from),
+            );
+            let cmd = &res[0];
+
+            match cmd.as_str() {
+                "describe" => self.parquet_handler.clone().describe().await,
+                "select" => self
+                    .parquet_handler
+                    .clone()
+                    .query(input.clone().to_string())
+                    .await
+                    .unwrap(),
+                "" => (),
+                "quit" => return,
+                "exit" => return,
+                _ => println!("Invalid command {:?}", input),
+            };
+        }
+        return;
+    }
+    pub fn new(ph: crate::parquethandler::ParquetHandler) -> Self {
+        let s = Self {
+            root_path: ph.pm.root_path.clone(),
+            parquet_handler: ph,
+        };
+        return s;
+    }
+}
